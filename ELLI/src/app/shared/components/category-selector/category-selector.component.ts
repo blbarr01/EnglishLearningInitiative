1ffabe2cb88566226ece1041dae840181f62e005
Brian Barr <bbarr0101@gmail.com>
2022-11-21 10:55:37 -0800

    ELLI-24 #time 10m #comment created the folders for the shared components; used ng generate to create the flashcard and category select component

---
+++ ELLI/src/app/shared/components/category-selector/category-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-selector',
+  templateUrl: './category-selector.component.html',
+  styleUrls: ['./category-selector.component.css']
+})
+export class CategorySelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
