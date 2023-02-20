e21d9e0a918b0129770b370aa39351b82145d517
Brian <bbarr0101@gmail.com>
2023-02-20 09:51:23 -0800

    began template for site map

--- ELLI/src/app/features/site-map/site-map.component.ts
+++ ELLI/src/app/features/site-map/site-map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-site-map',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SiteMapComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router) { }
+  
   ngOnInit(): void {
   }
 
