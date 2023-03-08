e5026d9ed1f3639e7b61a40feeba116d61761603
Brian <bbarr0101@gmail.com>
2023-03-08 08:54:06 -0800

    organizes imports in app.module.ts; seperates into library imports and user-generated imports

--- ELLI/src/app/app.module.ts
+++ ELLI/src/app/app.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { GameComponent } from './features/game/game.component';
-import { HomeComponent } from './features/home/home.component';
-import { LearnComponent } from './features/learn/learn.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestBed } from '@angular/core/testing';
-import { TestComponent } from './features/test/test.component';
-import { TalkToTextComponent } from './features/talk-to-text/talk-to-text.component';
-import { LoginComponent } from './features/login/login.component';
-import { SignupComponent } from './features/signup/signup.component';
-import { CategorySelectorComponent } from './shared/components/category-selector/category-selector.component';
-import { FlashcardComponent } from './shared/components/flashcard/flashcard.component';
-import { FormsModule } from '@angular/forms';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { AboutUsComponent } from './features/about-us/about-us.component';
-import { SiteMapComponent } from './features/site-map/site-map.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideFunctions,getFunctions } from '@angular/fire/functions';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { TestBed } from '@angular/core/testing';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './features/game/game.component';
+import { HomeComponent } from './features/home/home.component';
+import { LearnComponent } from './features/learn/learn.component';
+import { TestComponent } from './features/test/test.component';
+import { TalkToTextComponent } from './features/talk-to-text/talk-to-text.component';
+import { LoginComponent } from './features/login/login.component';
+import { SignupComponent } from './features/signup/signup.component';
+import { CategorySelectorComponent } from './shared/components/category-selector/category-selector.component';
+import { FlashcardComponent } from './shared/components/flashcard/flashcard.component';
+import { AboutUsComponent } from './features/about-us/about-us.component';
+import { SiteMapComponent } from './features/site-map/site-map.component';
 import { LessonsComponent } from './features/lessons/lessons.component';
 import { PersonsComponent } from './features/lessons/persons/persons.component';
 
