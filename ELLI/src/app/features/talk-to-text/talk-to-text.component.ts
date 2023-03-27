265fe9e84a58500a5a450604252bff42607ce41f
nidhipatel2001 <95837664+nidhipatel2001@users.noreply.github.com>
2023-03-27 00:44:46 -0700

    established code for speech recognition

--- ELLI/src/app/features/talk-to-text/talk-to-text.component.ts
+++ ELLI/src/app/features/talk-to-text/talk-to-text.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { useDeviceLanguage } from 'firebase/auth';
 
-
 @Component({
   selector: 'app-talk-to-text',
   templateUrl: './talk-to-text.component.html',
@@ -12,15 +11,58 @@ export class TalkToTextComponent implements OnInit {
   public interimTranscript: string = '';
   public finalTranscript: string = '';
   public isRecording: boolean = false;
+  private recognition: SpeechRecognition;
 
+  constructor(private router: Router) {
+    this.recognition = new webkitSpeechRecognition();
 
-  constructor(private router: Router) { 
+    // Set recognition options
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+
+    // Add event listeners
+    this.recognition.onstart = () => {
+      this.isRecording = true;
+    };
+
+    this.recognition.onerror = (event) => {
+      console.error(event.error);
+    };
+
+    this.recognition.onresult = (event) => {
+      let interimTranscript = '';
+      let finalTranscript = '';
+
+      for (let i = event.resultIndex; i < event.results.length; i++) {
+        const transcript = event.results[i][0].transcript;
+
+        if (event.results[i].isFinal) {
+          finalTranscript += transcript;
+        } else {
+          interimTranscript += transcript;
+        }
+      }
+
+      this.interimTranscript = interimTranscript;
+      this.finalTranscript = finalTranscript;
+    };
+
+    this.recognition.onend = () => {
+      this.isRecording = false;
+    };
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
+  // seech recognition started 
+  start(){
+    if(this.recognition) {
+    this.recognition.start();}
   }
 
-  
+  // speech recognition stopped
+  stop(){
+  if(this.recognition) {
+    this.recognition.stop();}
 }
-
+}
