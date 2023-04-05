ea47691eca7adcc57887fd1fd0a9d9bdd2ca40e4
nidhipatel2001 <95837664+nidhipatel2001@users.noreply.github.com>
2023-04-05 11:51:33 -0700

    deleted web-speech-api
    deleted web-speech-api

--- ELLI/src/app/features/talk-to-text/talk-to-text.component.ts
+++ ELLI/src/app/features/talk-to-text/talk-to-text.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { useDeviceLanguage } from 'firebase/auth';
-import { SpeechRecognition } from 'web-speech-api';
 
 @Component({
   selector: 'app-talk-to-text',
