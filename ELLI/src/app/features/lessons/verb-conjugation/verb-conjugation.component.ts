4df1fb4895815e0f4da2a2e58edd052126902b98
Brian <bbarr0101@gmail.com>
2023-04-10 16:03:07 -0700

    adds interactive verb slider

--- ELLI/src/app/features/lessons/verb-conjugation/verb-conjugation.component.ts
+++ ELLI/src/app/features/lessons/verb-conjugation/verb-conjugation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from "@angular/router"
 @Component({
   selector: 'app-verb-conjugation',
   templateUrl: './verb-conjugation.component.html',
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerbConjugationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  slideText  ={
+    "past": "I studied",
+    "present": "I study",
+    "future": "I will study"
+  }
+  displaytext:string = this.slideText["past"]  
+
+  changeDisplayText(input:string):void{
+    this.displaytext = input
+   }
+
+   showDashboard(){
+    this.router.navigateByUrl("/lessons")
+  }
+   
 }
