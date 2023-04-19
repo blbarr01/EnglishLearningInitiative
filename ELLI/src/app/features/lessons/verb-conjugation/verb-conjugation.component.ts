7d8f63b6c35134260dbccbceb5d34d540d601b3a
Brian <bbarr0101@gmail.com>
2023-04-19 08:36:32 -0700

    adds practice section for verb conjugation activity

--- ELLI/src/app/features/lessons/verb-conjugation/verb-conjugation.component.ts
+++ ELLI/src/app/features/lessons/verb-conjugation/verb-conjugation.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router"
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
+
 @Component({
   selector: 'app-verb-conjugation',
   templateUrl: './verb-conjugation.component.html',
@@ -7,11 +11,27 @@ import {Router} from "@angular/router"
 })
 export class VerbConjugationComponent implements OnInit {
 
+  practiceForm!: FormGroup;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-  }
 
+
+  }
+  userAnswers = {
+    1:"",
+    2:"",
+    3:"",
+    4:"",
+    5:""
+  }; 
+  answerKey = {
+    1:"am",
+    2:"are",
+    3:"is",
+    4:"are",
+    5:"are"
+  }
   slideText  ={
     "past": "I studied",
     "present": "I study",
@@ -26,5 +46,8 @@ export class VerbConjugationComponent implements OnInit {
    showDashboard(){
     this.router.navigateByUrl("/lessons")
   }
-   
+   logAnswers(){
+    console.log(this.userAnswers);
+    
+   }
 }
